8de9ade857a3ae7794cab41de2827f804f24ee63
ginger <gtsueng@gmail.com>
2021-10-28 13:39:12 -0700

    fix typo

--- parser.py
+++ parser.py
@@ -14,7 +14,7 @@ logger = config.logger
 
 import pathlib
 script_path = pathlib.Path(__file__).parent.absolute()
-with open(scriptpath+'append_misc_meta.py','w+') as appendfile:
+with open(os.path.join(script_path,'append_misc_meta.py'),'w+') as appendfile:
     r = requests.get('https://raw.githubusercontent.com/gtsueng/outbreak_misc_meta/main/append_misc_meta.py')
     appendfile.write(r.text)
     appendfile.close()
