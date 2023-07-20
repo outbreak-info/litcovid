7d7bd3693431aa1fb2ff345a7303cbf7ae83146c
Everaldo <everaldorodrigo@gmail.com>
2023-07-20 12:28:15 -0700

    Remove code to migrate to biothings.api 0.12.x (#27)

--- upload.py
+++ upload.py
@@ -35,7 +35,6 @@ class LitCovidUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
         }
     }
     idconverter = None
-    storage_class = biothings.hub.dataload.storage.BasicStorage
 
     def load_data(self, data_folder):
         self.logger.info("Load data from directory: '%s'" % data_folder)
